d92d5b89a978a78cf339cc23bf008bd182a1ae5a
Codemaster Mick <51538065+codemastermick@users.noreply.github.com>
2022-02-16 12:21:22 -0500

    fix(prettier-config): correct pathing issues

--- packages/prettier-config/lib/updatePackageFile.js
+++ packages/prettier-config/lib/updatePackageFile.js
@@ -1,13 +1,14 @@
 const fs = require("fs");
-var npmAddScript = require('npm-add-script');
-const packageFile = require('../package.json');
+const packagePath = '../../../package.json';
+const npmAddScript = require('npm-add-script');
+const packageFile = require(packagePath);
 
 packageFile.prettier = '@antlered-viking/prettier-config';
 npmAddScript({ key: "format", value: 'prettier --ignore-path --plugin-search-dir=. .', force: true });
 npmAddScript({ key: "format:fix", value: 'prettier --ignore-path --write --plugin-search-dir=. .', force: true });
 
 try {
-    fs.writeFileSync('package.json', JSON.stringify(packageFile, null, 2));
+    fs.writeFileSync(packagePath, JSON.stringify(packageFile, null, 2));
 } catch (error) {
     console.error(error);
 }
